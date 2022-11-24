50e484b2a93720d4054c18db76921acfe41d08b1
cfit-ss <85338848+cfit-ss@users.noreply.github.com>
2022-11-24 13:37:12 +0800

    Update RedisConfig.java

--- src/main/java/com/cips/data/Redis/RedisConfig.java
+++ src/main/java/com/cips/data/Redis/RedisConfig.java
@@ -22,8 +22,7 @@ public class RedisConfig {
         //为了方便开发，使用<String,Object>
         RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
         template.setConnectionFactory(factory);
-
-
+        /*序列化字段*/
         //json序列化配置
         //Jackson2JsonRedisSerializer jackson2JsonRedisSerializer =new Jackson2JsonRedisSerializer(Object.class);
         //json序列化方式
@@ -34,6 +33,23 @@ public class RedisConfig {
         om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
         jackson2JsonRedisSerializer.setObjectMapper(om);
 
+
+        /*进程目录地址--设置进程id字段*/
+        //进程目录
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer_1 = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer_1.setObjectMapper(om);
+        StringRedisSerializer stringRedisSerializer_1 =new StringRedisSerializer();
+
+        /*序列化 反序列化--反序列化
+        template.setKeySerializer(stringRedisSerializer_1);//序列化封装
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        //hash  序列化jackson--- 序列化参数字段
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        return  template;
+        */
         //String的序列化
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
 
