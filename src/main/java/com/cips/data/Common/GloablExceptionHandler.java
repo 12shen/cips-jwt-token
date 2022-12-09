403a2934d8d9854a3c6aff7fb546f132d9a7d2b2
cfit-ss <85338848+cfit-ss@users.noreply.github.com>
2022-12-09 09:42:32 +0800

    Create GloablExceptionHandler.java

--- src/main/java/com/cips/data/Common/GloablExceptionHandler.java
+++ src/main/java/com/cips/data/Common/GloablExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.cips.data.Common;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -19,9 +20,14 @@ public class GloablExceptionHandler {
         return Result.setResult(ResultEnum.UNKNOWN_ERROR);
     }
 
+
+
     /**-------- 空指针异常 --------**/
     @ExceptionHandler(NullPointerException.class)
     @ResponseBody
+    //responesBody---
+    //返回字段
+
     public Result error(NullPointerException e) {
         log.error("空指针异常：" + e.getMessage());
         System.out.println("空指针异常：" + e.getMessage());
@@ -42,6 +48,13 @@ public class GloablExceptionHandler {
         System.out.println("运行异常：" + e.getMessage());
         return Result.setResult(ResultEnum.SYSTEM_ERROR);
     }
+    /*空指针异常处理机制
+
+    public Result error(Throwable e){
+        log.error("运行异常："+e.getMessage());
+        System.out.pronln("exception:"+e.getMessage());
+        return Result.setResult(ResultEnum.SYSTEM_ERROR);
+    }*/
 
     /**-------- 自定义异常 --------**/
     @ExceptionHandler(CustomerDefinedException.class)
@@ -50,7 +63,7 @@ public class GloablExceptionHandler {
         log.error("自定义异常：" + e.getMessage());
         return Result.error().message(e.getMessage()).code(e.getCode());
     }
-
+//Exception--异常io
     /**-------- 算法异常 --------**/
     @ExceptionHandler(ArithmeticException.class)
     @ResponseBody
@@ -59,12 +72,9 @@ public class GloablExceptionHandler {
         return Result.error().message(e.getMessage());
         //return Result.error().message(e.getMassage()); 返回字段信息
     }
-/**创建对象的四种方式
- * new
- * clone
- * 反序列化
- *
+/**创建对象
  * */
+//
     /**-------- HTTP请求异常 --------**/
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     @ResponseBody
@@ -72,7 +82,6 @@ public class GloablExceptionHandler {
         log.error("HTTP请求异常：" + e.getMessage());
         return Result.error().message(e.getMessage());
     }
-
     /**-------- 数据库操作异常 --------**/
     @ExceptionHandler(DataAccessException.class)
     @ResponseBody
@@ -80,5 +89,19 @@ public class GloablExceptionHandler {
         log.error("数据库操作异常：" + e.getMessage());
         return Result.error().message(e.getMessage());
     }
-
+    /*
+    //HTTP请求异常----exception
+   public Result error_support(HttpRequestMethodNotSupportedException e){
+       log.error("mysql-operation exception"+e.getMessage());
+       return Result.error().message(e.getMessage());
+   }//生成异常状态
+    //状态转换
+    public Result error(DataAccessException e){
+        log.error("数据库异常："+e.getMessage());// uat
+        return Result.error().message(e.getMessage());
+    }*/
 }
+//参数字段-----
+ //参数字段匹配
+
+
